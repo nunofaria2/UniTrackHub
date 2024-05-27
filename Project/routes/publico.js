d4f963ec1655e37ae9badc1377be97e0d72b5ed3
nunofaria2 <nunofaria855@gmail.com>
2024-05-27 10:50:41 +0100

    .

--- Project/routes/publico.js
+++ Project/routes/publico.js
@@ -2,6 +2,13 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
+
+router.get('/index', (req, res) => {
+    const filePath = path.join(__dirname, '../frontEnd/Index.html');
+    console.log('Serving:', filePath); // Log para verificar o caminho
+    res.sendFile(filePath);
+  });
+
 // Endpoint para a página de login
 router.get('/login', (req, res) => {
   const filePath = path.join(__dirname, '../frontEnd/Login.html');
@@ -22,5 +29,23 @@ router.get('/forgotpassword', (req, res) => {
   console.log('Serving:', filePath); // Log para verificar o caminho
   res.sendFile(filePath);
 });
+
+router.get('/main', (req, res) => {
+    const filePath = path.join(__dirname, '../backOffice/Main.html');
+    console.log('Serving:', filePath); // Log para verificar o caminho
+    res.sendFile(filePath);
+  });
+
+  router.get('/horario', (req, res) => {
+    const filePath = path.join(__dirname, '../backOffice/Horario.html');
+    console.log('Serving:', filePath); // Log para verificar o caminho
+    res.sendFile(filePath);
+  });
+
+  router.get('/horariosturmas', (req, res) => {
+    const filePath = path.join(__dirname, '../backOffice/horariosturmas.html');
+    console.log('Serving:', filePath); // Log para verificar o caminho
+    res.sendFile(filePath);
+  });
 
 module.exports = router;
--- Project/server.js
+++ Project/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 
-const router = require('./routes/index');
+const router = require('./routes/publico');
 
 const app = express();
 app.use(bodyParser.json());
