63c14a47e804d5840ccce635e4a2d962a856706b
nunofaria2 <nunofaria855@gmail.com>
2024-05-27 10:37:07 +0100

    .

---
+++ Project/routes/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+// Endpoint para a página de login
+router.get('/login', (req, res) => {
+  const filePath = path.join(__dirname, '../frontEnd/Login.html');
+  console.log('Serving:', filePath); // Log para verificar o caminho
+  res.sendFile(filePath);
+});
+
+// Endpoint para a página de registro
+router.get('/register', (req, res) => {
+  const filePath = path.join(__dirname, '../frontEnd/Register.html');
+  console.log('Serving:', filePath); // Log para verificar o caminho
+  res.sendFile(filePath);
+});
+
+// Endpoint para a página de recuperação de senha
+router.get('/forgotpassword', (req, res) => {
+  const filePath = path.join(__dirname, '../frontEnd/ForgotPass.html');
+  console.log('Serving:', filePath); // Log para verificar o caminho
+  res.sendFile(filePath);
+});
+
+module.exports = router;
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# UniTrackHub
+
+```
+UniTrackHub
+└─ Project
+   ├─ .env
+   ├─ assets
+   │  └─ Img
+   │     ├─ ESTG.webp
+   │     └─ UniTrackHub.png
+   ├─ backOffice
+   │  ├─ Horario.html
+   │  ├─ Horario.json
+   │  ├─ HorariosTurmas.html
+   │  └─ Main.html
+   ├─ controllers
+   ├─ frontEnd
+   │  ├─ ForgotPass.html
+   │  ├─ Index.html
+   │  ├─ Login.html
+   │  └─ Register.html
+   ├─ Login.html
+   ├─ package-lock.json
+   ├─ package.json
+   ├─ README.md
+   ├─ routes
+   │  └─ index.js
+   ├─ server.js
+   └─ styles
+      ├─ Index.css
+      └─ Main.css
+
+```
