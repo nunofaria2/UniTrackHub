6c19a97bed46447d723224d750245dff764f0431
nunofaria2 <nunofaria855@gmail.com>
2024-06-04 10:33:27 +0100

    .

--- Project/server.js
+++ Project/server.js
@@ -3,13 +3,17 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 
-const router = require('./routes/publico');
+const publicoRouter = require('./routes/publico');
+//const routerMsql = require('./routes/msql');
+const privadoRouter = require('./routes/privado');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/api/', router);  // Caminho base para as rotas
+app.use('/api/', publicoRouter);
+app.use('/pri/', privadoRouter);  
+//app.use('/api/msql', routerMsql);  
 
 const port = process.env.SERVER_PORT || 8080;
 app.listen(port, () => {
